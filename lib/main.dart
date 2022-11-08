8ec234aafb397f63da8af107dab05cfeb85895f3
gitpher <jun_chois@naver.com>
2022-11-08 16:20:10 +0900

    instagram page w/ body filled by contents

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 // 배운 것
 // Image.asset() 과 Image.network()
 // leading, actions[], title
+// fit: BoxFit.cover
+// Row()
+// Spacer()는 이것 양쪽에 있는 요소의 사이 공간을 비게 만듬
+// MainAxisAlignment / CrossAxisAlignment
 
 void main() {
   runApp(MyApp());
@@ -45,7 +49,64 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Center(child: Text('Hello Homepage')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // 이미지
+          Image.network(
+            "https://media.bunjang.co.kr/product/200602626_1_1664367537_w360.jpg",
+            height: 400,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(CupertinoIcons.heart, color: Colors.black),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(CupertinoIcons.chat_bubble, color: Colors.black),
+                onPressed: () {},
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(CupertinoIcons.bookmark, color: Colors.black),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          // 좋아요
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "15300 likes",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // 설명
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Host Danaka's 2022 profile picture~ Hope you enjoy!!",
+            ),
+          ),
+
+          // 날짜
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "NOVEMBER 8",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
