1edcf59bfe5141d70896d9f5e55a7482c5ebb924
gitpher <jun_chois@naver.com>
2022-11-08 15:33:52 +0900

    simple instagram appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+// 배운 것
+// Image.asset() 과 Image.network()
+// leading, actions[], title
 
 void main() {
   runApp(MyApp());
@@ -22,6 +27,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+        title: Image.asset(
+          'assets/logo.png',
+          height: 32,
+        ),
+      ),
       body: Center(child: Text('Hello Homepage')),
     );
   }
