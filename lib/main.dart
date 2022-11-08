098087340fe6165789c5f8551c72e71a04a6a1df
gitpher <jun_chois@naver.com>
2022-11-08 15:19:55 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Text('Hello Homepage')),
+    );
   }
 }
